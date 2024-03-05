b5522d953f4e9b71e3cfb99431f95e49e902b6ca
Murillo <murillovipcampeao@gmail.com>
2024-03-05 01:26:51 -0300

    'Dash - Source code'

--- teste.py
+++ teste.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.express as px
 
 dados = 'dados_biodisel.csv'
 df = pd.read_csv(dados, delimiter=',')
@@ -26,12 +25,14 @@ st.set_page_config(
     layout='wide', 
     page_title='Dashboard Vendas Biodiesel - 2017 a 2024', 
     initial_sidebar_state='collapsed', 
-    menu_items={'About': '# This is a project made for my Data *Analysis classes*'})
+    menu_items = {'About': '# This is a project made for my Data *Analysis classes*'})
 
 st.markdown(background, unsafe_allow_html=True)
 
 df['Mês/Ano'] = pd.to_datetime(df['Mês/Ano'].str.strip(), utc=False)
+
 df['Ano'] = df['Mês/Ano'].dt.year
+
 df['Vendas de Biodiesel'] = df['Vendas de Biodiesel'].str.replace(',', '').astype(float).astype(int)
 
 df_agrupado = df.groupby(df['Ano']).agg({'Vendas de Biodiesel': 'sum'}).reset_index()
@@ -43,13 +44,12 @@ df = df.drop(df.columns[-1], axis=1)
 for index, row in df_agrupado.iterrows():
     ano = str(row['Ano'])
     df_vendas_x_ano[ano] = row['Vendas de Biodiesel']
+    df_vendas_x_ano['Vendas de Biodiesel'] = df['Vendas de Biodiesel'].astype(int)
 
 st.sidebar.header("Vendas de Biodiesel Petrobrás")
 st.sidebar.caption("Vendas de biodiesel Petrobrás entre 01/2017 a 01/2024")
 
 if st.sidebar.button("Clique para visualizar"):
-    st.header("Vendas totais de biodiesel - (2017 - 2024)")
-    fig = px.bar(df_agrupado, x='Ano', y='Vendas de Biodiesel', title="Vendas Biodiesel (Em Bilhões)")
-    st.plotly_chart(fig)
+    st.header("Soma das vendas totais de biodiesel por ano (2017 - 2024)")
+    st.plotly_chart(df_vendas_x_ano)
     st.sidebar.code(df)
-    st.dataframe(df)
